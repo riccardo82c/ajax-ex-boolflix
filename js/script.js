e9ec89f36b2888f91d6751dd30ec9de0b71cdea1
Riccardo Colombo <rikyregna@gmail.com>
2020-09-02 16:38:34 +0200

    prova

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
+/* 
+Milestone 1:
+	Creare un layout base con una searchbar(una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film.
+	Possiamo, cliccando il bottone, cercare sull’ API tutti i film che contengono ciò che ha scritto l’ utente.
+	Vogliamo dopo la risposta dell’ API visualizzare a schermo i seguenti valori per ogni film trovato:
+	Titolo
+	Titolo Originale
+	Lingua
+	Voto
+ */
+
+
 $(function () {
-
-
 
 	$('#input').keypress(function (e) {
 		if (e.which == 13) {
-			$('#list').empty();
-			let title = $("#input").val();
-			retrieveString(title);
-			$("#input").val('');
+			retrieveString();
 		}
 	});
 
-
-
 	$("#search-btn").click(function () {
-		$('#list').empty();
-		let title = $("#input").val();
-		retrieveString(title);
-		$("#input").val('');
+		retrieveString();
+
 	});
 
 	/* funzione */
-
-	function retrieveString(str) {
+	function retrieveString() {
+		$('#list').empty();
+		let str = $("#input").val();
+		$("#input").val('');
 
 		$.ajax({
 			method: 'GET',
@@ -37,22 +42,27 @@ $(function () {
 				var source = $("#entry-template").html();
 				var template = Handlebars.compile(source);
 
+				if (obj.results.length > 0) {
 
-				for (let i = 0; i < obj.results.length; i++) {
+					for (let i = 0; i < obj.results.length; i++) {
 
-					let hbObj = {
-						title: obj.results[i].title,
-						original_title: obj.results[i].original_title,
-						original_language: obj.results[i].original_language,
-						vote_average: obj.results[i].vote_average
+						let hbObj = {
+							title: obj.results[i].title,
+							original_title: obj.results[i].original_title,
+							original_language: obj.results[i].original_language,
+							vote_average: obj.results[i].vote_average
+						}
+
+						var html = template(hbObj);
+						$('#list').append(html);
 					}
-					console.log(hbObj);
-					var html = template(hbObj);
-					$('#list').append(html);
+				} else {
+					$('#list').append('il titolo ricercato non esiste');
 				}
 
 			},
 			error: function () {
+				alert('Errore');
 
 			}
 		});
