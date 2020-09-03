75e875521f49b7bdd837de85812685edd51a9aba
Riccardo Colombo <rikyregna@gmail.com>
2020-09-03 15:30:07 +0200

    2 MILESTONE half done

--- js/script.js
+++ js/script.js
@@ -7,28 +7,57 @@ Milestone 1:
 	Titolo Originale
 	Lingua
 	Voto
+
+Milestone 2:
+	Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote(troviamo le icone in FontAwesome).
+	Arrotondiamo sempre per eccesso all’ unità successiva, non gestiamo icone mezze piene(o mezze vuote: P)
+	Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’ API(le flag non ci sono in FontAwesome).
+	Allarghiamo poi la ricerca anche alle serie tv.Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili(le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+	Qui un esempio di chiamata per le serie tv:
+	https: //api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
  */
 
 
 $(function () {
 
-	$('#input').keypress(function (e) {
-		if (e.which == 13) {
-			retrieveString();
+	$('#input').keydown(function (e) {
+		if (e.which == 13 && e.keyCode == 13 && $('#input').val()) {
+			findCollection();
 		}
 	});
 
 	$("#search-btn").click(function () {
-		retrieveString();
-
+		if ($('#input').val()) {
+			findCollection();
+			$('#input').focus();
+		}
 	});
 
-	/* funzione */
-	function retrieveString() {
+
+
+	/* FUNZIONI */
+	// chiamata AJAX per restituire la stringa
+	function findCollection() {
+		// setto a vuoto il placeholder prima della query ajax
+		$('#input').attr('placeholder', '');
+		// salvo in una variabile il campo preso da #input e resetto
+		let query = saveAndReset();
+		// chiamata AJAX
+		ajaxCall(query);
+	}
+
+
+	// resetto il DOM, SALVO valore di input e svuoto input 
+	function saveAndReset() {
 		$('#list').empty();
-		let str = $("#input").val();
+		let value = $("#input").val();
 		$("#input").val('');
 
+		return value
+	}
+
+	// chiamata AJAX principale per ritornare i valori di ogni item
+	function ajaxCall(str) {
 		$.ajax({
 			method: 'GET',
 			url: 'https://api.themoviedb.org/3/search/movie',
@@ -38,28 +67,15 @@ $(function () {
 				query: str,
 			},
 			success: function (obj) {
-
-				var source = $("#entry-template").html();
-				var template = Handlebars.compile(source);
-
-				if (obj.results.length > 0) {
-
-					for (let i = 0; i < obj.results.length; i++) {
-
-						let hbObj = {
-							title: obj.results[i].title,
-							original_title: obj.results[i].original_title,
-							original_language: obj.results[i].original_language,
-							vote_average: obj.results[i].vote_average
-						}
-
-						var html = template(hbObj);
-						$('#list').append(html);
-					}
+				// se ci sono risultati
+				if (obj.total_results > 0) {
+					printCollection(obj);
 				} else {
-					$('#list').append('il titolo ricercato non esiste');
+					/* posso sennò aggiungere una funzione no result */
+					// se nn ci sono titoli 
+					$('#list').append('Il titolo ricercato non esiste');
+					$('#input').attr('placeholder', 'Sorry, no result');
 				}
-
 			},
 			error: function () {
 				alert('Errore');
@@ -67,16 +83,73 @@ $(function () {
 			}
 		});
 
-
-
-
 	}
 
 
+	// inserisco i movie nel DOM
+	function printCollection(data) {
+		/* tutto questo posso metterlo in una funzione esterna */
+		// template HB
+		var source = $("#entry-template").html();
+		var template = Handlebars.compile(source);
+		for (let i = 0; i < data.results.length; i++) {
 
+			let hbObj = {
+				title: data.results[i].title,
+				original_title: data.results[i].original_title,
+				original_language: langFlags(data.results[i].original_language),
+				vote_average: star(data.results[i].vote_average)
+			}
 
+			var html = template(hbObj);
+			$('#list').append(html);
 
+			// setto il placeholder
+			$('#input').attr('placeholder', 'Insert Title');
+		}
+	}
 
+	// funzione per convertire il numero in stelle
+	function star(int) {
+		int = Math.round(int / 2);
+		const starFull = '<i class="fas fa-star"></i>';
+		const starEmpty = '<i class="far fa-star"></i>';
+		let result = starFull.repeat(int) + starEmpty.repeat(5 - int);
+		return result;
+	}
+
+	// funzione per trasformare un numero da 1 - 10 a 1 - 5 stelle
+	function toStar(int) {
+		return Math.round(int / 2);
+	}
+
+	// funzione per stampare le stelline corrispondenti
+	function printStar(int, location) {
+		const starFull = '<i class="fas fa-star"></i>';
+		const starEmpty = '<i class="far fa-star"></i>';
+		let result = starFull.repeat(int) + starEmpty.repeat(5 - int)
+		location.append(result);
+	}
+
+	// funzione che ritorna l'immagine se presente sennò ritorna la stringa iniziale
+	function langFlags(str) {
+		switch (str) {
+			case 'en':
+				return ('img/en.png');
+			case 'it':
+				return ('img/it.png');
+			case 'fr':
+				return ('img/fr.png');
+			case 'de':
+				return ('img/de.png');
+			case 'es':
+				return ('img/es.png');
+			case 'ja':
+				return ('img/ja.png');
+			default:
+				return str
+		}
+	}
 
 
 });
